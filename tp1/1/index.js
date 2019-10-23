81d9ab4b3eb87bb279fab39e449da9a7f36ad5a3
AntoninDemaneche <43409991+AntoninDemaneche@users.noreply.github.com>
2019-10-23 11:15:44 +0200

    Update index.js

--- tp1/1/index.js
+++ tp1/1/index.js
@@ -7,6 +7,40 @@ exports = module.exports = {
   datasets,
   algo: function (input) {
     // YOUR CODE BETWEEN HERE
+    let x = input[0];
+    console.log(input)
+    
+    for (let i = 1; i < input.length; i++){
+      let tab = input[i].split(' ');
+        
+        
+        /*
+        for( i = 0; i < input; i2++ ){
+          x = x + i 
+        }
+        for( i = 1; i < input.lenght; i3++ ){
+          x = x - i;
+        }
+        console.log(x)
+        */
+        
+    }
+    console.log(tab)
+    console.log(x)
+
+
+
+
+    /*
+    console.log(x)
+    if ( 10000 > x > 101 )
+    return(100)
+    if (x < 10000)
+    return('KO')
+    if(x > 100)
+    return(1000)
+    */
+
 
     // AND HERE
   },
@@ -18,3 +52,10 @@ exports = module.exports = {
     }
   }
 }
+
+/* 
+Si fin 100> = 1 000 
+Si Fin 10 000> = 100 
+Si Fin <10 000 = 0 
+
+*/
